f94fa11e3f22c0f927b99d5d44471ee7b425516f
EdwardZombie <edward-chuprov@mail.ru>
2022-08-26 00:01:36 +0700

    Товары на главной.

--- pages/_app.js
+++ pages/_app.js
@@ -3,24 +3,25 @@ import Layout from '../component/ui/Layout'
 import '../styles/css/plugins.css'
 import '../styles/css/main.css'
 
-const MyApp = ({ Component, pageProps }) => (
+const MyApp = ({ Component, pageProps }) => {
+    return(
     <Layout>
 
-      <Head>
-          <meta charSet="utf-8"/>
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-          <title>Pustok - Book Store HTML Template</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Head>
+            <meta charSet="utf-8"/>
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+            <title>Pustok - Book Store HTML Template</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-          {/*<link rel="stylesheet" type="text/css" media="screen"  href="/css/plugins.css" />*/}
+            {/*<link rel="stylesheet" type="text/css" media="screen"  href="/css/plugins.css" />*/}
 
 
-
-      </Head>
-      <main>
-        <Component {...pageProps} />
-      </main>
+        </Head>
+        <main>
+            <Component {...pageProps} />
+        </main>
     </Layout>
-);
+    )
+}
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,78 @@
-import Hero from "../component/ui/indexpage/Hero";
 import Shiping from "../component/ui/indexpage/Shiping";
-import Deal from "../component/ui/indexpage/Deal";
+import Link from "next/link";
+import React from "react";
+import Image from "next/image";
 
+export const getStaticProps = async () => {
+    const responce = await fetch('http://prokansk.ru/api/v1/itemsforindex/');
+    const data = await responce.json();
 
-export default function Home() {
+    if(!data) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { tovarsMain: data },
+    }
+}
+
+const myLoader = ({ src, width, quality }) => {
+    return `http://prokansk.ru${src}?w=${width}&q=${quality || 75}`
+}
+
+const Home = ({ tovarsMain }) => {
   return (
     <>
-    {/*<Hero />*/}
-      <Shiping/>
-      {/*<Deal/>*/}
+
+            <div className="container">
+                <div className="row">
+
+
+                        {tovarsMain && tovarsMain.map(({id, name, price, image1}) => (
+                            <div className="col-lg-3 col-md-4 mb-lg--60 mb--30" key={id}>
+                                <div className="blog-card card-style-grid">
+
+                                    <Link href="#">
+                                        <a className="promo-image sidebar">
+                                            <Image
+                                                loader={myLoader}
+                                                src={image1}
+                                                alt={name}
+                                                width={350}
+                                                height={250}
+                                            />
+                                        </a>
+                                    </Link>
+
+
+                                    <div className="card-content">
+                                        <h3 className="title">
+                                            <Link href="#">
+                                                <a>{name}</a>
+                                            </Link>
+                                        </h3>
+                                        <p className="post-meta">
+                                            <span>{price} руб.</span>
+                                        </p>
+
+                                    </div>
+
+                                </div>
+                            </div>
+                        ))}
+
+
+
+                </div>
+            </div>
+
+        
+        
+        <Shiping/>
     </>
   )
 }
+
+export default Home;
